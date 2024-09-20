976f78e34895907cc284396b28379dabbf12da7c
dilshod <dilshodx516@gmail.com>
2024-09-20 06:53:24 +0500

    masala

---
+++ app.js
@@ -0,0 +1,42 @@
+// N1
+
+// {
+//     function centerNum(arr) {
+//         const leng = arr.length;
+
+//         if (leng % 2 === 1) {
+//             return arr[Math.floor(leng / 2)]
+//         }
+//         const center1 = arr[leng / 2 - 1]
+//         const center2 = arr[leng / 2]
+//         return (center1 + center2) / 2
+//     }
+//     console.log(centerNum([1, 2, 3, 4, 5]));
+//     console.log(centerNum([1, 2, 3, 4,]));
+// }
+
+
+// N2
+
+// {
+//     function tub(num) {
+//         if (num === 2) {
+//             return `${num} tub son`;
+
+//         }
+//         if (num <= 1) {
+//             `${num} tub son emas`
+
+//         }
+//         for (let i = 2; i <= Math.sqrt(num); i++) {
+//             if (num % 2 === 0) {
+//                 `${num} tub son emas`
+
+//             }
+//         }
+//         return `${num} tub son`;
+
+//     }
+//     console.log(tub(2));
+
+// }
